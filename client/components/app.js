c91d9741510740b935730b14d4ab9129d597cbdf
Richard Yu <ryu323@gmail.com>
2016-05-15 12:13:32 -0700

    add search bar template

--- client/components/app.js
+++ client/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         Hello World!
+        <SearchBar />
       </div>
     );
   }
